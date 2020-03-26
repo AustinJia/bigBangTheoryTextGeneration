d1ab1b9da0b68370d1fbdb44c49b1419b67f48bf
Raghav Raj Mittal <raj.raghav@gmail.com>
2020-03-26 20:41:22 +0530

    save model

--- torch_utils.py
+++ torch_utils.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 import torch.nn as nn
+import helper
 
 def batch_data(words, sequence_length, batch_size):
     """
